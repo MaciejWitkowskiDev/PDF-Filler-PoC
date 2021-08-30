84db2c0a5401328734441be8705ae5e156c0646d
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-08-30 13:03:34 +0200

    implemented the fieldmapper function

---
+++ FieldMapper.py
@@ -0,0 +1,23 @@
+class InvalidMappingException(Exception):
+    pass
+
+class FieldMapper:
+
+    def __generateDict__(values : dict, mapping : dict):
+        ret = dict()
+        for field, key in mapping.items():
+            ret[field] = values[key]
+        return ret 
+
+    def __verifyMapping__(values : dict, mapping : dict):
+        for key in values.keys():
+            if key not in mapping.keys():
+                raise InvalidMappingException(f"Invalid mapping provided: value of {key} not mapped.")
+        return True
+
+    def __init__(self, values : dict, mapping : dict):
+        if self.__verifyMapping__(mapping, values):
+            self.__valuedict = self.__generateDict__(values, mapping)
+    
+    def getMapping(self):
+        return self.__valuedict
