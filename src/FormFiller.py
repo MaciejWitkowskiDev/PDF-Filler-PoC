1a81a8b7a0205c07a1e8fd6ec794542281e0b9ae
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-08-30 16:04:01 +0200

    Implemented a simple filler class

---
+++ src/FormFiller.py
@@ -0,0 +1,35 @@
+# For more flexibility, the FormFiller constructor takes in an already read byte array of the  pdf file, NOT a python File object!
+import pdfrw
+from CONSTS import *
+from io import BytesIO
+from FieldMapper import FieldMapper
+class FormFiller:
+
+    def __fill_pdf__(input_pdf_filestream, data_dict):
+        template_pdf = pdfrw.PdfReader(input_pdf_filestream)
+        for page in template_pdf.pages:
+            annotations = page[ANNOT_KEY]
+            if annotations:
+                for annotation in annotations:
+                    if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                        if annotation[ANNOT_FIELD_KEY]:
+                            key = annotation[ANNOT_FIELD_KEY][1:-1]
+                            if key in data_dict.keys():
+                                if type(data_dict[key]) == bool:
+                                    if data_dict[key] == True:
+                                        annotation.update(pdfrw.PdfDict(
+                                            AS=pdfrw.PdfName('Yes')))
+                                else:
+                                    annotation.update(
+                                        pdfrw.PdfDict(V='{}'.format(data_dict[key]))
+                                    )
+                                    annotation.update(pdfrw.PdfDict(AP=''))
+        return template_pdf
+
+    def fillForm(self,mapper : FieldMapper):
+        value_mapping = mapper.getValues()
+        filled_pdf : pdfrw.PdfReader = self.__fill_pdf__(self.filesteam, value_mapping)
+        
+
+    def __init__(self, filestream : bytes):
+        self.filesteam : bytes = filestream
