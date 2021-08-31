094ec038877bccad2ce2dd34f8c48aef696be80a
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-08-31 14:55:57 +0200

    added external json config file, renamed main.py

--- src/PdfGenerator.py
+++ src/PdfGenerator.py
@@ -1,17 +1,19 @@
 from io import BytesIO
+from json import load as json_load
 from FormFiller import FormFiller
 from FieldMapper import FieldMapper
 
 def main():
+    
     with open("test.pdf", "rb") as f:
         filestream = BytesIO(f.read())
+    
+    with open('mapping.json') as json_file:
+        mapping = json_load(json_file)['pit-example']
+    
     values = {
         'nip' : '1172204485',
         'urzad': 'Urząd skarbowy Łódź Widzew'
-    }
-    mapping = {
-        'p_1' : 'nip',
-        'p_6' : 'urzad'
     }
     mapper = FieldMapper(values,mapping)
     formfill = FormFiller(filestream)
