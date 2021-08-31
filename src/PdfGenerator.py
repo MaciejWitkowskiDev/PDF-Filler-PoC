53170a4742d7679f085afecb8e9da1c7b1723c4d
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-08-31 16:06:07 +0200

    Created the PdfGenerator class

--- src/PdfGenerator.py
+++ src/PdfGenerator.py
@@ -1,25 +1,29 @@
 from io import BytesIO
 from json import load as json_load
+from os import path as os_path
 from FormFiller import FormFiller
 from FieldMapper import FieldMapper
 
-def main():
-    
-    with open("test.pdf", "rb") as f:
-        filestream = BytesIO(f.read())
-    
-    with open('mapping.json') as json_file:
-        mapping = json_load(json_file)['pit-example']
-    
-    values = {
-        'nip' : '1172204485',
-        'urzad': 'Urząd skarbowy Łódź Widzew'
-    }
-    mapper = FieldMapper(values,mapping)
-    formfill = FormFiller(filestream)
-    formfill.fillForm(mapper)
-    new = formfill.getFilestream()
-    with open('output.pdf','wb') as f:
-        f.write(new.getbuffer())
+class InvalidFormException(Exception):
+    pass
+class PdfGenerator:
+    def __init__(self):
+        if not os_path.isfile("mapping.json"):
+            raise FileNotFoundError("Mapping file not found!")
+        else:
+            with open("mapping.json") as field_mapping:
+                self.__field_mapping = json_load(field_mapping)
 
-main()
+    def getMapping(self):
+        return self.__field_mapping
+
+    def generatePdf(self, form : str, values: dict):
+        if form in self.getMapping().keys():
+            with open(f"{form}.pdf", "rb") as pdf_template:
+                filestream = BytesIO(pdf_template.read())            
+                mapper = FieldMapper(values, self.getMapping()[form])
+                filler = FormFiller(filestream)
+                filler.fillForm(mapper)
+                return filler.getFilestream()
+        else:
+            raise InvalidFormException(f"Mapping for form {form} not found!")
