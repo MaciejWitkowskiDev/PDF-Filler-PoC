8cf1886476c0b9ad3024e97aca41bea4c9ba5525
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-11-17 11:39:14 +0100

    added a more elaborate script

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
-<?php
-
+<?php 
 function generatePdf($form, $fields){
-    $command_string = "python3 generate_pdf.py ".$form." ";
+    $command_string = "python generate_pdf.py ".$form." ";
     foreach ($fields as $key => $value) {
         $command_string .= $key.":".$value." ";
     }
@@ -15,18 +14,54 @@ function generatePdf($form, $fields){
         error_log("Python PDF generator error. Returned message: ".$response["message"]);
         die();
     } else {
-        $fileContent = $response["response_body"];
+        $fileContent = base64_decode($response["response_body"]);
         header('Content-Type: application/pdf');
-        echo base64_decode($fileContent);
+        header('Content-Length: '.strlen( $fileContent ));
+        header('Content-disposition: attachment; filename="' . $form . '.pdf"');
+        #CONFIGURE THIS LATER
+        header('Cache-Control: public, must-revalidate, max-age=0');
+        header('Pragma: public');
+        echo $fileContent;
     }
 }
 
-generatePdf("PIT-37", array(
-    'rok' => '2021',
-    'krs' => '989898989898',
-    'cel' => rawurlencode('PXTest00001'),
-    'agreement' => True
-));
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $file = $_POST["file"]; 
+    if(isset($_POST["year"])) {
+        $year = $_POST["year"];
+    }
+    if(isset($_POST["krs"])) {
+        $krs = $_POST["krs"];
+    }
+    if(isset($_POST["cel"])) {
+        $cel = $_POST["cel"];
+    }
+    
+} else  {
+    $file = $_GET["file"]; 
+    if(isset($_GET["year"])) {
+        $year = $_GET["year"];
+    }
+    if(isset($_GET["krs"])) {
+        $krs = $_GET["krs"];
+    }
+    if(isset($_GET["cel"])) {
+        $cel = $_GET["cel"];
+    }
+}
 
-exit;
-?>
+if(isset($cel) && isset($krs)){
+    generatePdf($file, array(
+        'rok' => $year,
+        'krs' => $krs,
+        'cel' => rawurlencode($cel),
+        'agreement' => 'Tak'
+    ));
+} else {
+    generatePdf($file, array(
+        'rok' => $year,
+    ));
+}
+
+flush(); // This is essential for large downloads 
+?> 
